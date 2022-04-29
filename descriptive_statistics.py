567f5c48b1ab763596669feb8aae4849e751ad24
The Ancient One <100685110+SophrosyneEunoia@users.noreply.github.com>
2022-04-30 00:25:13 +0700

    add : variance

--- descriptive_statistics.py
+++ descriptive_statistics.py
@@ -6,10 +6,12 @@ class Desc:
 
     Available Function
     1. Mean
+    2. Varians
     """
 
     def __init__(self, *nums):
         self.nums = nums
+        self.len = len(nums)
 
     def mean(self):
         """
@@ -17,7 +19,17 @@ class Desc:
         >>> print(p1.mean())
         2.0
         """
-        return sum(self.nums) / len(self.nums)
+        return sum(self.nums) / self.len
+
+    def variance(self):
+        """
+        >>> p1 = Desc(1,2,3)
+        >>> print(p1.variance())
+        1.0
+        """
+        rt = Desc.mean(self.nums)
+        li = [(nu - rt)**2 for num in self.nums]
+        return sum(li) / self.len
 
 if __name__ == '__main__':
     import doctest
