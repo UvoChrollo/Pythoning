47d2c8e82d27739c5b86018acbccd0e89d2cd6da
The Ancient One <100685110+SophrosyneEunoia@users.noreply.github.com>
2022-04-30 00:12:58 +0700

    let me love you

---
+++ activation_sigmoid.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def sigmoid(vector : np.array) -> np.array:
+    """
+    >>> sigmoid(np.array([-1.0, 1.0, 2.0]))
+    array([0.26894142, 0.73105858, 0.88079708])
+
+    >>> sigmoid(np.array([0.0]))
+    array([0.5])
+    """
+    return 1 / (1 + np.exp(-vector))
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
